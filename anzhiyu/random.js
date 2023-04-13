9cf42c70c7a1cf3587111280e1c091f8bf559333
geekswg <geekswg@users.noreply.github.com>
2023-04-13 16:37:51 +0800

    deploy: 9e460bcc95f6aa3d09a6d6f414e12b38ae5aa745

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/04/11/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/04/13/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
