9294914a64d7c3ea1705e459c43eb3e12e827534
geekswg <geekswg@qq.com>
2023-06-04 20:44:08 +0800

    🚑

--- source/custom/custom.js
+++ source/custom/custom.js
@@ -12,3 +12,10 @@ const Blog = {
 
 
 }
+
+
+// 自定义 metingjs-api
+// https://api.injahow.cn/meting/ 
+// https://meting.qjqq.cn/
+// https://meting.anzhiy.cn/?server=netease&type=playlist&id=8152976493&auth=undefined&r=0.367
+var meting_api='https://meting.anzhiy.cn/?server=:server&type=:type&id=:id&auth=:auth&r=:r';
