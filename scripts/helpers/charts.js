4bec07b231d253f915b449b63b94030ee904ddb0
GavinWells <31149899+geekswg@users.noreply.github.com>
2023-06-09 03:52:26 +0800

    update

--- scripts/helpers/charts.js
+++ scripts/helpers/charts.js
@@ -395,3 +395,53 @@ function categoriesChart (dataParent) {
     });
   </script>`
 }
+
+
+function switchPostChart () {
+  // 这里为了统一颜色选取的是“明暗模式”下的两种字体颜色，也可以自己定义
+  let color = document.documentElement.getAttribute('data-theme') === 'light' ? '#4C4948' : 'rgba(255,255,255,0.7)'
+  if (document.getElementById('posts-chart') && postsOption) {
+    try {
+      let postsOptionNew = postsOption
+      postsOptionNew.title.textStyle.color = color
+      postsOptionNew.xAxis.nameTextStyle.color = color
+      postsOptionNew.yAxis.nameTextStyle.color = color
+      postsOptionNew.xAxis.axisLabel.color = color
+      postsOptionNew.yAxis.axisLabel.color = color
+      postsOptionNew.xAxis.axisLine.lineStyle.color = color
+      postsOptionNew.yAxis.axisLine.lineStyle.color = color
+      postsOptionNew.series[0].markLine.data[0].label.color = color
+      postsChart.setOption(postsOptionNew)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  if (document.getElementById('tags-chart') && tagsOption) {
+    try {
+      let tagsOptionNew = tagsOption
+      tagsOptionNew.title.textStyle.color = color
+      tagsOptionNew.xAxis.nameTextStyle.color = color
+      tagsOptionNew.yAxis.nameTextStyle.color = color
+      tagsOptionNew.xAxis.axisLabel.color = color
+      tagsOptionNew.yAxis.axisLabel.color = color
+      tagsOptionNew.xAxis.axisLine.lineStyle.color = color
+      tagsOptionNew.yAxis.axisLine.lineStyle.color = color
+      tagsOptionNew.series[0].markLine.data[0].label.color = color
+      tagsChart.setOption(tagsOptionNew)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  if (document.getElementById('categories-chart') && categoriesOption) {
+    try {
+      let categoriesOptionNew = categoriesOption
+      categoriesOptionNew.title.textStyle.color = color
+      categoriesOptionNew.legend.textStyle.color = color
+      if (!categoryParentFlag) { categoriesOptionNew.series[0].label.color = color }
+      categoriesChart.setOption(categoriesOptionNew)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+document.getElementById("mode-button").addEventListener("click", function () { setTimeout(switchPostChart, 100) })
